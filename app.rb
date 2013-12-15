2dc429e1462e133dbd38396ea810a1e152752e0f
sranso <sranso@gmail.com>
2013-12-15 16:07:20 -0500

    update results page to tell a story

--- app.rb
+++ app.rb
@@ -9,6 +9,10 @@ class Weird < Sinatra::Application
   end
 
   post '/' do
+    @monogamous_y = "They can't imagine life with anyone else."
+    @monogamous_n = "They prefer to have multiple lovers."
+    @intmdng_y = "On the other hand, #{params["family"]["siblings"]["0"]["name"]} was not so loving, and in fact bullied #{params["vegname"]}."
+    @intmdng_n = "Similarly, #{params["family"]["siblings"]["0"]["name"]} was a loving sibling, and they frolicked in the vegetable patch often."
     erb :results
   end
 end
